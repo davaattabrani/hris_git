573105f38c647fd066438156e4ecde6b7c41ba52
rayhanrhm04 <rayhancb45@gmail.com>
2023-08-14 21:51:25 +0700

    menmbuat tampilan menu sesuai role

--- app/Http/Controllers/HalamanController.php
+++ app/Http/Controllers/HalamanController.php
@@ -12,9 +12,7 @@ class HalamanController extends Controller
         return view('dashboard');
     }
     function pm(){
-        echo "Halo selamat datang di halaman project manager";
-        echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='/logout'>Logout >></a>";
+        return view('dashboard');
     }
     function sa(){
         echo "Halo selamat datang di halaman system analyst";
@@ -29,11 +27,11 @@ class HalamanController extends Controller
     function qa(){
         echo "Halo selamat datang di halaman Quality Assurance";
         echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='logout'>Logout >></a>";
+        echo "<a href='/logout'>Logout >></a>";
     }
     function programmer(){
         echo "Halo selamat datang di halaman Programmer";
         echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='logout'>Logout >></a>";
+        echo "<a href='/logout'>Logout >></a>";
     }
 }
